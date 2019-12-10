4580980336ced7e339276431ebfe4b54c913dab2
Calmynt <mctech26@gmail.com>
2019-12-10 08:37:11 +0100

    Added health

---
+++ JarheadPro/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    private int health = 100;
+    public Text healthText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "HEALTH : " + health;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+        }
+    }
+}
