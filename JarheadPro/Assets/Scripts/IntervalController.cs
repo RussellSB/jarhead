55b863d6a775fc7e481d983432d42966142171d9
RussellSB <r.sammutbonnici@gmail.com>
2020-01-09 12:34:10 +0100

    IntervalController for Months

---
+++ JarheadPro/Assets/Scripts/IntervalController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalController : MonoBehaviour
+{
+    private GameObject player;
+    Vector3 initPos;
+    float prevX;
+    float displacementX;
+
+    public float intervalLength = 100f;
+    public static float distanceTillNextCheckpoint;
+    public static int intervalCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        initPos = player.transform.position;
+        prevX = player.transform.position.x;
+
+        distanceTillNextCheckpoint = intervalLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(player.transform.position.x != prevX)
+        {
+            displacementX = player.transform.position.x - prevX;
+            distanceTillNextCheckpoint -= displacementX;
+            prevX = player.transform.position.x;
+
+            if (distanceTillNextCheckpoint <= 0)
+            {
+                distanceTillNextCheckpoint = intervalLength;
+                intervalCount++;
+            }
+
+            //Debug.Log(displacementX + ", " + distanceTillNextCheckpoint + ", " + intervalCount);
+        }
+
+
+       
+    }
+}
---
+++ JarheadPro/Assets/Scripts/Month.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Month : MonoBehaviour
+{
+    public Text month;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        month.text = string.Format("{0:0}", IntervalController.intervalCount);
+    }
+}
